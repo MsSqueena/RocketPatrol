528ca6659290b2677af64eeac7082a762ee15e8d
JacquelineCastro <jcastr30@ucsc.edu>
2021-04-15 21:04:54 -0700

    Collision Works

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -37,6 +37,7 @@ class Play extends Phaser.Scene {
         keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
         keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
     }
+    
     update (){
         this.starfield.tilePositionX -= 4;
         this.p1Rocket.update();
@@ -45,5 +46,28 @@ class Play extends Phaser.Scene {
         this.ship01.update();
         this.ship02.update();
         this.ship03.update();
+
+        //check collisions
+        if (this.checkCollision(this.p1Rocket, this.ship03)){
+            this.p1Rocket.reset();
+            this.ship03.reset();
+        }
+        if (this.checkCollision(this.p1Rocket, this.ship02)){
+            this.p1Rocket.reset();
+            this.ship02.reset();
+        }
+        if (this.checkCollision(this.p1Rocket, this.ship01)){
+            this.p1Rocket.reset();
+            this.ship01.reset();
+        }
+    }
+
+    checkCollision(rocket,ship){
+        //simple AABB(Axis-Aligned Bounding Boxes) checking
+        if (rocket.x < ship.x + ship.width && rocket.x + rocket.width > ship.x && rocket.y < ship.y + ship.height && rocket.height + rocket.y > ship.y) {
+                return true;
+        } else {
+            return false;
+        }
     }
 }
